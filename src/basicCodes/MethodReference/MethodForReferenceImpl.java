48f6da3031173336daa19cff4cc7d40b5db4dc58
subhram biswas <subhrambiswas2014@gmail.com>
2023-03-02 22:41:47 +0530

    java 8 method referencing and stream

---
+++ src/basicCodes/MethodReference/MethodForReferenceImpl.java
@@ -0,0 +1,10 @@
+package basicCodes.MethodReference;
+
+public class MethodForReferenceImpl implements MethodForReference {
+
+
+    @Override
+    public int add(int a, int b) {
+        return a+b;
+    }
+}
